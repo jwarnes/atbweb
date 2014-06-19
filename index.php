2ccacadd79ed64adeffec78d2f5f6f314514b2ec
Justin Warnes <jwarnes@samaritan.org>
2014-06-19 16:43:09 -0400

    let's get rolling

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php $page = $_GET['page'] ?:  'people'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <!-- Vendor -->
+        <link rel="stylesheet" href="semantic/css/semantic.css">
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+        <script src="semantic/javascript/semantic.js"></script>
+        <script src="http://knockoutjs.com/downloads/knockout-3.1.0.debug.js"></script>
+        <script src="js/vendor/jquery.scrollTo.min.js"></script>
+        <script src="js/vendor/jquery.sticky.js"></script>
+        <link rel="stylesheet" href="css/styles.css">
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <div class="ui large inverted vertical sidebar menu borderless">
+            <div class="item header"><img src="img/atb3.png" height="90%" width="90%"></div>
+            <div class="header item">Roster <i class="icon users"></i></div>
+            <a class="item" href="index.php?page=people">People</a>
+            <a class="item" href="index.php?page=characters">Characters</a>
+            <a class="item" href="index.php?page=teams">Teams</a>
+            <div class="header item">Communication <i class="icon chat"></i></div>
+            <a class="item" href="index.php?page=applications">Applications</a>
+            <a class="item" href="index.php?page=relationships">Relationships</a>
+            <a class="item" href="index.php?page=incidents">Incidents</a>
+            <div class="header item">Reports <i class="basic icon bar chart"></i></div>
+            <a class="item" href="index.php?page=attendance">Attendance</a>
+            <a class="item" href="index.php?page=performance">Performance</a>
+            <a class="item" href="index.php?page=loot">Loot</a>
+        </div>
+        <div class="ui fixed main menu black inverted transparent">
+            
+            <div class="ui black launch button float left item">
+                <i class="list layout icon"></i>
+                <span class="small text">Menu</span>
+            </div>
+            <div class="container">
+                <div class="title item">ATB Web Toolkit</div>
+                <div class="item small">Version 1</div>
+                <div class="right menu" id="app-search">
+                    <div class="item">
+                        <div class="ui icon input small">
+                            <input type="text" placeholder="Search...">
+                            <i class="search inverted icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "$page.php"; ?>
+    </body>
+    <script>
+    $('.sidebar').sidebar();
+    $('.launch').click(function(){
+    $('.sidebar').sidebar('toggle');
+    });
+    $('.ui.dropdown')
+    .dropdown()
+    ;
+    var page = <?php echo "'$page'"; ?>;
+    $(".sidebar a.item:contains('"+page.capitalize()+"')").addClass("active");
+    $('.scroll').click(function(){
+    var to = '#' + $(this).attr('to');
+    $.scrollTo(to,{offset: {top: -100}, duration: 300});
+    $('.scroll').removeClass('active');
+    
+    $(this).addClass('active');
+    });
+    $('.sticky').sticky({topSpacing: 44});
+    </script>
+</html>
---
+++ people.php
@@ -0,0 +1,46 @@
+<div class="top content">
+    <div class="container">
+        <h1 class="ui dividing header"><i class="icon user"></i>People</h1>
+        <p>Manage the humans known to the guild, inside or out.</p>
+        <div class="ui segment"><i>You can find meanness in the least of creatures, but when God made man the devil was at his elbow. A creature that can do anything. Make a machine. And a machine to make the machine. And evil that can run itself a thousand years, no need to tend it.</i></div>
+    </div>
+</div>
+<div class="main container">
+    <h2 class="ui dividing header">Add a new contact</h2>
+    <div class="ui form segment">
+        <div class="two fields">
+            <div class="field">
+                <label>Nickname</label>
+                <div class="ui left labeled input">
+                    <input type="text" placeholder="Nickname">
+                    <div class="ui corner label"><i class="icon asterisk"></i></div>
+                </div>
+            </div>
+            <div class="field">
+                <label>Real name</label>
+                <div class="ui left input">
+                    <input type="text" placeholder="Real name">
+                </div>
+            </div>
+        </div>
+        <div class="field">
+            <label>Gender</label>
+            <div class="ui fluid selection dropdown">
+                <div class="text">Select</div>
+                <i class="dropdown icon"></i>
+                <input type="hidden" name="gender">
+                <div class="menu">
+                    <div class="item" data-value="male">Male</div>
+                    <div class="item" data-value="female">Female</div>
+                    <div class="item" data-value="cyborg">Cyborg</div>
+                </div>
+            </div>
+        </div>
+        <div class="ui blue submit button icon big">
+            <i class="icon add sign box"></i>
+            Add New
+        </div>
+    </div>
+</div>
+<script>
+</script>
